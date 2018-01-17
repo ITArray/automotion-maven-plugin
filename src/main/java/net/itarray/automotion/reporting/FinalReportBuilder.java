4d0f9137417b32cf831c38db3ecfe93c29a72d9c
Denys Zaiats <denys.zaiats@gmail.com>
2018-01-17 15:37:32 +0100

    [master] - init commit

--- src/main/java/net/itarray/automotion/reporting/FinalReportBuilder.java
+++ src/main/java/net/itarray/automotion/reporting/FinalReportBuilder.java
@@ -11,7 +11,6 @@ import com.webfirmframework.wffweb.tag.html.lists.Ol;
 import com.webfirmframework.wffweb.tag.html.metainfo.Head;
 import com.webfirmframework.wffweb.tag.htmlwff.NoTag;
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 
 import java.io.BufferedOutputStream;
@@ -21,14 +20,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-@Mojo(name = "automotion")
+@Mojo(name = "automotion-report")
 public class FinalReportBuilder extends AbstractMojo {
 
     private static final String TARGET_AUTOMOTION = "target/automotion/";
     private static final String TARGET_AUTOMOTION_HTML = TARGET_AUTOMOTION + "html/";
 
     @Override
-    public void execute() throws MojoExecutionException {
+    public void execute() {
         File folder = new File(TARGET_AUTOMOTION_HTML);
         File[] listOfFiles = folder.listFiles();
         List<String> files = new ArrayList<>();
@@ -55,34 +54,34 @@ public class FinalReportBuilder extends AbstractMojo {
     private Html buildHtml(final List<String> files) {
         return new Html(null,
                 new Style("background-color: #fff")) {{
-                super.setPrependDocType(true);
-                new Head(this) {{
-                        new TitleTag(this) {{
-                            new NoTag(this, "Automotion report");
-                        }};
-                        new NoTag(this, "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-                        new NoTag(this, "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no\">");
+            super.setPrependDocType(true);
+            new Head(this) {{
+                new TitleTag(this) {{
+                    new NoTag(this, "Automotion report");
+                }};
+                new NoTag(this, "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+                new NoTag(this, "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no\">");
 
-                        new NoTag(this, "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js\"></script>");
-                        new NoTag(this, "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">");
-                        new NoTag(this, "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css\" integrity=\"sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp\" crossorigin=\"anonymous\">");
-                        new NoTag(this, "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\" integrity=\"sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa\" crossorigin=\"anonymous\"></script>");
-                    }};
-
-                new Body(this) {{
-                        new Ol(this) {{
-                                for (final String s: files) {
-
-                                    new Li(this) {{
-
-                                            new A(this,
-                                                    new Href("html/" + s)) {{
-                                                        new NoTag(this, s);
-                                            }};
-                                        }};
-                                }
-                            }};
-                    }};
+                new NoTag(this, "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js\"></script>");
+                new NoTag(this, "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">");
+                new NoTag(this, "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css\" integrity=\"sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp\" crossorigin=\"anonymous\">");
+                new NoTag(this, "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\" integrity=\"sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa\" crossorigin=\"anonymous\"></script>");
             }};
+
+            new Body(this) {{
+                new Ol(this) {{
+                    for (final String s : files) {
+
+                        new Li(this) {{
+
+                            new A(this,
+                                    new Href("html/" + s)) {{
+                                new NoTag(this, s);
+                            }};
+                        }};
+                    }
+                }};
+            }};
+        }};
     }
 }
